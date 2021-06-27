c9c496dd3bffcdd07cac0929b45c22d43c083521
github username <d24dust@hotmail.com>
2021-06-27 13:27:56 -0400

    add: is_valid_project? & project_name methods

--- config/route_helper.rb
+++ config/route_helper.rb
@@ -10,4 +10,12 @@ class RouteHelper
     @project_name = @req.params["PROJECT_NAME"]
   end
 
+  def is_valid_project?
+    !!@req.get? && @@valid_project_names.include?(@project_name)
+  end
+
+  def project_name
+    @@valid_project_names.include?(@project_name) ? @project_name : nil
+  end
+
 end

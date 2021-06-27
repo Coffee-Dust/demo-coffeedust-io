fa5b1fb011cb4170ddd8f43c19a43ffe2a36abf4
github username <d24dust@hotmail.com>
2021-06-27 14:16:49 -0400

    add: default project directory

--- lib/models/project_demo.rb
+++ lib/models/project_demo.rb
@@ -6,6 +6,8 @@ class ProjectDemo
     attr_hash.each do |key, value|
       self.send("#{key}=", value)
     end
+    self.dir = ProjectDemoConfig.default_directory + self.name if !self.dir
+  end
 
   end
   

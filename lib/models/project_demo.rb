7a90624a1521b9620b34d2d079945c9b3d7e3bc2
github username <d24dust@hotmail.com>
2021-06-27 14:03:47 -0400

    add: mass assignment on #init

--- lib/models/project_demo.rb
+++ lib/models/project_demo.rb
@@ -2,7 +2,10 @@ class ProjectDemo
   
   attr_accessor :name, :dir, :port, :pid
 
-  def initialize()
+  def initialize(attr_hash)
+    attr_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
 
   end
   

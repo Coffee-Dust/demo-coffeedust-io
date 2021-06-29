7088b4f9792b5d19f84ab85e1533a93a2bd35dc3
github username <d24dust@hotmail.com>
2021-06-29 13:20:08 -0400

    setup: ProjectsConfig initialization

--- config/environment.rb
+++ config/environment.rb
@@ -8,3 +8,12 @@ require_relative './route_helper.rb'
 require_relative '../lib/models/project_demo.rb'
 require_relative '../lib/projects_controller.rb'
 require_relative '../lib/application.rb'
+
+ProjectsConfig.new do |config|
+  config.default_directory = "/project_forks"
+  
+  config.project_instances = [
+    ProjectDemo.new(name: "coffee_talk_rails", port: 6900, config: config),
+    ProjectDemo.new(name: "LCCT", port: 6901, config: config)
+  ]
+end

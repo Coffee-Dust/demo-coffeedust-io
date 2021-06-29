55cb49d10202d6e09727c5ac668d6e246164e1f1
github username <d24dust@hotmail.com>
2021-06-29 13:25:33 -0400

    update: require_relative filename

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require 'bundler/setup'
 Bundler.require(:default, :development)
 
 require 'thread'
-require_relative './project_demo_config.rb'
+require_relative './projects_config.rb'
 require_relative './route_helper.rb'
 require_relative '../lib/models/project_demo.rb'
 require_relative '../lib/projects_controller.rb'

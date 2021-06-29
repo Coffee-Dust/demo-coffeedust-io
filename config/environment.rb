0f4176dea92bee2c14a47df91cae3901a9d4af90
github username <d24dust@hotmail.com>
2021-06-29 12:11:21 -0400

    require: relative files

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,9 @@ require 'bundler/setup'
 
 Bundler.require(:default, :development)
 
+require 'thread'
+require_relative './project_demo_config.rb'
+require_relative './route_helper.rb'
+require_relative '../lib/models/project_demo.rb'
+require_relative '../lib/projects_controller.rb'
 require_relative '../lib/application.rb'

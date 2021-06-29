6ee7d9b7cd9bd7dc13d08ed3fac77be2b537d62d
github username <d24dust@hotmail.com>
2021-06-29 15:11:13 -0400

    feat: class.start method

--- lib/projects_controller.rb
+++ lib/projects_controller.rb
@@ -1,3 +1,8 @@
 class ProjectsController
-  
+
+  def self.start(project_instance)
+    project_instance.start if !project_instance.isRunning?
+    return "Success! demo:#{project_instance.name} is running on port #{project_instance.port}"
+  end
+
 end

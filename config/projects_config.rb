e2d17ffd8ddae7bfdb7f8329afe92c3366c417d0
github username <d24dust@hotmail.com>
2021-06-29 12:07:14 -0400

    add: initialize & setter methods

--- config/projects_config.rb
+++ config/projects_config.rb
@@ -1,5 +1,17 @@
 class ProjectsConfig
 
-  
+
+  def initialize
+    yield(self)
+    @@project_names = self.class.project_instances.collect(&:name)
+  end
+
+  def project_instances=(array)
+    @@project_instances = array
+  end
+
+  def default_directory=(value)
+    @@default_directory = ENV["PWD"] + value
+  end
 
 end

3be8cd8cbde851f5150a67575231656a5f7177f8
github username <d24dust@hotmail.com>
2021-07-22 14:20:21 -0400

    change: return response to json with instancePort

--- lib/projects_controller.rb
+++ lib/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController
 
   def self.start(project_instance)
     project_instance.start if !project_instance.isRunning?
-    return "Success! demo:#{project_instance.name} is running on port #{project_instance.port}"
+    return "{\"instancePort\": #{project_instance.port}, \"instanceStartedWithPort\": true}"
   end
 
 end

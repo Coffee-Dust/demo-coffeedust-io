664c9cafe0165e67b2734236980a2aa211bb327b
github username <d24dust@hotmail.com>
2021-08-08 17:21:46 -0400

    update: start_command for local_coffeeshop to use puma instead of rackup

--- config/environment.rb
+++ config/environment.rb
@@ -16,6 +16,6 @@ ProjectsConfig.new do |config|
     ProjectDemo.new(name: "coffee_beans_rails", port: 6900),
     ProjectDemo.new(name: "my_coffee_table", port: 6901),
     ProjectDemo.new(name: "coffee_beans", port: 6902),
-    ProjectDemo.new(name: "local_coffeeshop", port: 6903, start_command: "rackup -E test -o 'ssl://0.0.0.0?key=#{ENV["DEMO_SERVER_SSL_KEY"]}&cert=#{ENV["DEMO_SERVER_SSL_CRT"]}'")
+    ProjectDemo.new(name: "local_coffeeshop", port: 6903, start_command: "puma -b 'ssl://0.0.0.0?key=#{ENV["DEMO_SERVER_SSL_KEY"]}&cert=#{ENV["DEMO_SERVER_SSL_CRT"]}'")
   ]
 end

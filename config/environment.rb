504f9d1a63b6eb7cf290ee614f4a96fe0a67cd70
github username <d24dust@hotmail.com>
2021-08-12 15:18:14 -0400

    remove: require_rel for route_helper.rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,6 @@ Bundler.require(:default, :development)
 require 'thread'
 require_relative '../lib/controllers/application_controller.rb'
 require_relative './projects_config.rb'
-require_relative './route_helper.rb'
 require_relative '../lib/models/project_demo.rb'
 require_relative '../lib/controllers/projects_controller.rb'
 

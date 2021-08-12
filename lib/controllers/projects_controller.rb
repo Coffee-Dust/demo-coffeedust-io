8866944ae6f3949d3f9d9093d7d2a98d666865f8
github username <d24dust@hotmail.com>
2021-08-12 14:46:38 -0400

    update: to sinatra syntax & refactor action

--- lib/controllers/projects_controller.rb
+++ lib/controllers/projects_controller.rb
@@ -1,8 +1,15 @@
 class ProjectsController < ApplicationController
 
-  def self.start(project_instance)
-    project_instance.start if !project_instance.isRunning?
-    return "{\"instancePort\": #{project_instance.port}, \"instanceStartedWithPort\": true}"
+  get '/start_server' do
+    project_instance = ProjectsConfig.find_instance_by_name(params["PROJECT_NAME"])
+    
+    if project_instance
+      project_instance.start if !project_instance.isRunning?
+      erb "{'instancePort': #{project_instance.port}, 'instanceStartedWithPort': true}"
+    else
+      status 469
+      erb "{'instanceStartedWithPort': false}"
+    end
   end
 
 end

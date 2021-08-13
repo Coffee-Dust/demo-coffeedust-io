3c0602ea5d82ac10e4b8524568b80f6dfac4f269
github username <d24dust@hotmail.com>
2021-08-13 14:22:13 -0400

    update: method call naming

--- lib/controllers/projects_controller.rb
+++ lib/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
 
   get '/start_server' do
-    project_instance = ProjectsConfig.find_instance_by_name(params["PROJECT_NAME"])
+    project_instance = ProjectDemos::Server.find_instance_by_name(params["PROJECT_NAME"])
     
     if project_instance
       project_instance.start if !project_instance.isRunning?

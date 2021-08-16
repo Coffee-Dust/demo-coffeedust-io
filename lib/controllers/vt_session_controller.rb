f183d7ab6da1ce49caf6cad29f44c34285a7c3f5
github username <d24dust@hotmail.com>
2021-08-16 14:35:31 -0400

    feat: POST/virtual_terminal/input action

--- lib/controllers/vt_session_controller.rb
+++ lib/controllers/vt_session_controller.rb
@@ -1,4 +1,17 @@
 class VTSessionController < ApplicationController
 
+  post "/virtual_terminal/input" do
+    if valid_session?
+      @terminal.send_input(params[:input])
+
+      cycles_elapsed = 0
+      unless @terminal.input_processed? || cycles_elapsed > 1000
+        cycles_elapsed += 1
+      end
+      content_type :json
+      {output_lines: @terminal.project.latest_output}.to_json
+
+    end
+  end
 
 end

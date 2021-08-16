1105fa306a270b7fd492a1227fef97bbb29d715c
github username <d24dust@hotmail.com>
2021-08-16 15:01:34 -0400

    add: getter/setter methods for thread variables: terminal, input_processed?, latest_output, output_history, input

--- lib/models/project_thread.rb
+++ lib/models/project_thread.rb
@@ -1,3 +1,37 @@
 class ProjectThread < Thread
   
+
+  def set_terminal(val)
+    self.thread_variable_set("terminal", val)
+  end
+
+  def terminal
+    self.thread_variable_get("terminal")
+  end
+
+  def input_processed?
+    self.thread_variable_get("input_processed")
+  end
+
+  def set_input_processed(val)
+    self.thread_variable_set("input_processed", val)
+  end
+
+  def latest_output
+    self.thread_variable_get("latest_output")
+  end
+
+  def add_to_latest_output_chunk(val)
+    self.thread_variable_set("latest_output", self.thread_variable_get("latest_output").push(val))
+  end
+
+  def add_output_chunk(val)
+    output_history = self.thread_variable_get("output_history")
+    self.thread_variable_set("output_history", output_history.push(val))
+  end
+
+  def input
+    self.thread_variable_get("input")
+  end
+
 end

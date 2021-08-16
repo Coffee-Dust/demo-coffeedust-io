8c1e37bfb52a5b81119cd43252e72ef909442501
github username <d24dust@hotmail.com>
2021-08-16 14:47:45 -0400

    feat: #run_inventory_manager method functional

--- lib/models/project_demos/CLI.rb
+++ lib/models/project_demos/CLI.rb
@@ -2,6 +2,23 @@ module ProjectDemos
 
   class CLI
 
+    def run_inventory_manager
+      require_relative "#{@@clone_directory}/inventory_manager/config/environment"
+      im = Inventory_Manager.new
+      c = Interface_Controller.new(im)
+
+      def c.puts(text)
+        Thread.current.on_project_puts(text)
+      end
+
+      def c.gets
+        Thread.current.on_project_gets
+      end
+
+      c.start_program_loop
+    end
+
+
     def self.start(name, thread)
       self.new(name, thread)
     end

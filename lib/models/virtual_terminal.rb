6be60bc4c57d633f51370e13fb4697dd2588016d
github username <d24dust@hotmail.com>
2021-08-16 14:52:02 -0400

    add: input_processed? shortcut method

--- lib/models/virtual_terminal.rb
+++ lib/models/virtual_terminal.rb
@@ -16,6 +16,10 @@ class VirtualTerminal
     @project.send_input(input)
   end
 
+  def input_processed?
+    @project.input_processed?
+  end
+
   def self.find_session(terminal_id)
     @@sessions.detect do |vt| 
       vt.id == terminal_id 

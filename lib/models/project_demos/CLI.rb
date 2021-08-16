d44eb301a950d4129b8fec8622cacf8a1ac369df
github username <d24dust@hotmail.com>
2021-08-16 14:45:01 -0400

    add: initialize method

--- lib/models/project_demos/CLI.rb
+++ lib/models/project_demos/CLI.rb
@@ -5,7 +5,12 @@ module ProjectDemos
     def self.start(name, thread)
       self.new(name, thread)
     end
-    
+
+    def initialize(name, thread)
+      @name = name
+      @thread = thread
+
+    end
 
     def self.config
       yield

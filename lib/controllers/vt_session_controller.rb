711f216b78a849dd86d4a5b808c6cf317741dc9f
github username <d24dust@hotmail.com>
2021-08-18 19:32:52 -0400

    change: post/input while loop structure

--- lib/controllers/vt_session_controller.rb
+++ lib/controllers/vt_session_controller.rb
@@ -5,8 +5,14 @@ class VTSessionController < ApplicationController
       @terminal.send_input(params[:input])
 
       cycles_elapsed = 0
-      while !@terminal.input_processed? && cycles_elapsed < 1000
+
+      while !@terminal.input_processed?
         cycles_elapsed += 1
+        # prevent inf loop if @terminal never processes input
+        if cycles_elapsed > 5000000
+          @terminal.project.thread_variable_set("latest_output", ["<ERROR> program timed out while processing request.", "Please try again"])
+          @terminal.project.set_input_processed(true)
+        end
       end
 
       content_type :json

ac5960047bf3dede3496d57d4a26f59f88ad319e
github username <d24dust@hotmail.com>
2021-08-19 13:05:05 -0400

    fix: now renders json format

--- lib/controllers/projects_controller.rb
+++ lib/controllers/projects_controller.rb
@@ -5,10 +5,12 @@ class ProjectsController < ApplicationController
     
     if project_instance
       project_instance.start if !project_instance.isRunning?
-      erb "{'instancePort': #{project_instance.port}, 'instanceStartedWithPort': true}"
+      content_type :json
+      {instancePort: project_instance.port, instanceStartedWithPort: true}.to_json
     else
       status 469
-      erb "{'instanceStartedWithPort': false}"
+      content_type :json
+      {instanceStartedWithPort: false}.to_json
     end
   end
 

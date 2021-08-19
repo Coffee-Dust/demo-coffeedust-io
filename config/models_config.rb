125f978e062091ecbbba1583aaa888f6d5c62b9f
github username <d24dust@hotmail.com>
2021-08-18 21:02:48 -0400

    fix: my_coffee_table instance not starting in ssl

--- config/models_config.rb
+++ config/models_config.rb
@@ -3,7 +3,7 @@ ProjectDemos::Server.config do
 
   @@project_instances = [
     ProjectDemos::Server.new(name: "coffee_beans_rails", port: 6900),
-    ProjectDemos::Server.new(name: "my_coffee_table", port: 6901, start_command: "rails s -e test"),
+    ProjectDemos::Server.new(name: "my_coffee_table", port: 6901),
     ProjectDemos::Server.new(name: "coffee_beans", port: 6902),
     ProjectDemos::Server.new(name: "local_coffeeshop", port: 6903, start_command: "puma -b 'ssl://0.0.0.0?key=#{ENV["DEMO_SERVER_SSL_KEY"]}&cert=#{ENV["DEMO_SERVER_SSL_CRT"]}'")
   ]
